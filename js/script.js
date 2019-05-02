142a1289ead85b68207219fc5b9584c53133cff0
Ignacio Cerna <ignaciocerna@ccpaedu.com>
2019-05-02 00:24:05 +0000

    Put a title on the seacrh bar

--- js/script.js
+++ js/script.js
@@ -1,8 +1,31 @@
 /* global $ */
 
-console.log('hi');
-
-$('h1').click(function(){
-    $('h1').css('color', 'green');
-    $('h1').text('Ready to Code');
+$("#search-button").click(function() {
+    var searchTerm = $("#search-term").val();
+//   $.ajax({
+//   url: "http://www.omdbapi.com/?apikey=13d0e91&s=" + searchTerm
+//       var url = response.data[0].images.original.url;
+//  $('body').append('<img src="' + url + '">');
+//   },
+//   error:function() {
+//       alert("error");
+//   }
+  
+// });
+$.ajax({
+          url: "https://www.omdbapi.com/?apikey=13d0e91&s=" + searchTerm,
+          method: "GET",
+          success: function(response) {
+               $(".Search").html("");
+              response.Search.forEach(function(movies){
+              
+             $(".Search").append("<img src=" + movies.Poster + ">"); 
+             $(".Search").append("<div> " + movies.Title + "</div>");    
+              })
+              //$('body').append(response)
+             
+          }
+          
+})
 });
+ 
